f8fc81e84fee0387069949584ff042f2411b580c
Ankush Mohammed <ankushece2012@gmail.com>
2023-05-22 13:50:07 +0530

    second commit

--- Pages/Login.py
+++ Pages/Login.py
@@ -9,10 +9,10 @@ class LoginPage:
         self.login_button = (By.ID, 'login-button')
 
     def enter_username(self, username):
-        self.driver.find_element(*self.username_field).send_keys(username)
+        self.driver.find_element(self.username_field).send_keys(username)
 
     def enter_password(self, password):
-        self.driver.find_element(*self.password_field).send_keys(password)
+        self.driver.find_element(self.password_field).send_keys(password)
 
     def click_login_button(self):
-        self.driver.find_element(*self.login_button).click()
+        self.driver.find_element(self.login_button).click()
